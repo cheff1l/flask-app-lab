5bf2d7975d2b45e035caaceb4b679d63dac49b6a
Denis Semkovych <denissemkovich1337@gmail.com>
2025-10-16 17:40:23 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return 'Hello, world!'
+@app.route('/resume')
+def resume():
+    return render_template('resume.html', title='Резюме')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html', title='Контакти')
 
 if __name__ == '__main__':
     app.run(debug=True)
