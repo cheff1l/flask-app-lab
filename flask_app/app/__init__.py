6448e8eb4b40c038fd6ad38a17375a79a61b6c5d
Denis Semkovych <denissemkovich1337@gmail.com>
2025-10-30 17:45:32 +0200

    Лабораторна робота №4: Об'єкт запиту request. Сесії та кукі.

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_pyfile("../config.py")
 
+app.secret_key = 'secret_key'
+
 from .users import users_bp
 from .products import products_bp
 
